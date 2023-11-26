997c486d40a15fd74eea3751f4999ae84d5b4c3a
Omar Akoudad <oakoudad@e2r9p10.1337.ma>
2023-11-26 22:35:07 +0100

    Done

--- srcs/requirements/mariadb/files/script.sh
+++ srcs/requirements/mariadb/files/script.sh
@@ -14,30 +14,3 @@ mysqladmin -u root --password=$MYSQL_ROOT_PASSWORD shutdown
 
 
 mysqld_safe
-
-
-#!/bin/bash
-
-# # Update the MySQL/MariaDB configuration
-# sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# # Start the MariaDB service
-# echo "01 ------"
-# service mariadb start
-
-# # Use 'mysql' command to set a password for the root user
-# echo "02 ------"
-# mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;"
-
-# # Create a database and user with privileges
-# mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
-# mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-# mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* to '$MYSQL_USER'@'%';"
-# mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-
-# echo "03 ------"
-# mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-
-# # Start the MariaDB service again
-# echo "04 ------"
-# mysqld_safe
--- srcs/requirements/wordpress/files/script.sh
+++ srcs/requirements/wordpress/files/script.sh
@@ -1,40 +1,27 @@
 #!/bin/bash
 
-#create folder
 mkdir -p /var/www/html
 cd /var/www/html
 
-#install wp
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-#more
+
 wp core download --allow-root
 
-# mv /wp-config.php /var/www/html/wp-config.php
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 wp config set DB_NAME $MYSQL_DB --allow-root
 wp config set DB_USER $MYSQL_USER --allow-root
 wp config set DB_HOST mariadb --allow-root
 wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root
-#update info sql connect mariadb
-# sed -i -r "s/mysql_db_here/$MYSQL_DB/1"   wp-config.php
-# sed -i -r "s/mysql_user_here/$MYSQL_USER/1"  wp-config.php
-# sed -i -r "s/mysql_password_here/$MYSQL_PASSWORD/1"    wp-config.php
 
-#config wordpress
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 wp plugin update --all --allow-root
 
-# change port in php
-#more
 sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
 chown -R www-data:www-data /var/www/html
-# run 
+
 /usr/sbin/php-fpm7.4 -F
-
-
-# chown -R root:root /var/www/html
