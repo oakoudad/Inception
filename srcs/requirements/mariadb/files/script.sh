adc4bb3196d508418629a8a9697ec00c47aef228
Omar Akoudad <oakoudad@e2r7p7.1337.ma>
2023-11-28 16:06:32 +0100

    done

--- srcs/requirements/mariadb/files/script.sh
+++ srcs/requirements/mariadb/files/script.sh
@@ -8,9 +8,7 @@ mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
 mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* to '$MYSQL_USER'@'%';"
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;"
-# mariadb -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root --password=$MYSQL_ROOT_PASSWORD shutdown
-
 
 mysqld_safe
