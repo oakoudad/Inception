b5b583c9b1d8ab6af20de5120148dbf4c4b59cdf
Omar Akoudad <oakoudad@e2r10p15.1337.ma>
2023-11-29 17:13:47 +0100

    Done

--- srcs/requirements/nginx/files/script.sh
+++ srcs/requirements/nginx/files/script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/nginx-selfsigned.key -out /etc/nginx/certs/nginx-selfsigned.crt -subj "/C=MA/ST=Agadir/L=Agadir/O=Mediaplus.ma/OU=DevOps/CN=localhost"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/certs/nginx-selfsigned.key -out /etc/nginx/certs/nginx-selfsigned.crt -subj "/C=MA/L=Agadir/O=Mediaplus.ma/OU=DevOps/CN=localhost"
